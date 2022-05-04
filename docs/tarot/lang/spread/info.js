12d79d86acf32cb6751c07578edf5a62ffa9bb1b
ken.yek <ken.yek@advantech.com.tw>
2022-05-04 14:45:12 +0800

    update spread info

---
+++ docs/tarot/lang/spread/info.js
@@ -0,0 +1,92 @@
+(function (global) {
+    const info = [{
+            type: 'one_card_spread',
+            num: 1,
+            name: 'one'
+        },
+        {
+            type: 'two_card_spread',
+            num: 2,
+            name: 'two'
+        },
+        {
+            type: 'three_card_spread',
+            num: 3,
+            name: 'three',
+            info: '① 代表過去的情況與狀況。<br>② 代表現在的狀態與處境。<br>③ 代表未來的走向與趨勢。'
+        },
+        {
+            type: 'four_card_spread',
+            num: 4,
+            name: 'four'
+        },
+        {
+            type: 'five_card_spread',
+            num: 5,
+            name: 'five'
+        },
+        {
+            type: 'five1_card_spread',
+            num: 5,
+            name: 'five1'
+        },
+        {
+            type: 'mental_healing',
+            num: 5,
+            name: 'mentalHealing'
+        },
+        {
+            type: 'five_card_cross',
+            num: 5,
+            name: 'fiveCross'
+        },
+        {
+            type: 'ellipse_spread',
+            num: 7,
+            name: 'ellipseSpread'
+        },
+        {
+            type: 'horseshoe',
+            num: 7,
+            name: 'horseshoe'
+        },
+        {
+            type: 'hexagram',
+            num: 7,
+            name: 'hexagram',
+            info: '① ：問題過去狀況。<br/>② ：問題現在狀況。<br/>③ ：問題未來狀況。<br/>④ ：解決此一問題的對應策略。<br/>⑤ ：目前的環境與周遭狀況。<br/>⑥ ：問卜者本人抱持的態度與心態。<br/>⑦ ：結果或可能發生的結局。'
+        },
+        {
+            type: 'work_dicision_making',
+            num: 9,
+            name: 'workDicisionMaking'
+        },
+        {
+            type: 'celtic_cross_spread',
+            num: 10,
+            name: 'celticCross',
+            info: '① ：現況：代表問題的現狀。<br/>② ：影響因素 : 表示加在現況上的阻力或助力。橫放的牌，圖案的正面頂端朝右為正位，反之正面頂端朝左，則為逆位。<br/>③ ：理想 : 表示問卜者覺得最理想的狀況，或是對於事情的想法。<br/>④ ：基礎 : 表示目前已累積的成果或可以利用的資源，以及問題的成因。<br/>⑤ ：過去 : 問題的過去狀況。<br/>⑥ ：未來 : 表示問卜者未來會發生的事情。<br/>⑦ ：自我 : 問卜者本身的狀況。<br/>⑧ ：環境 : 表示外在環境因素或他人的觀點。<br/>⑨ ：希望或恐懼 : 表示問卜者希望發生或害怕發生的事情 (正面的牌為希望發生，負面的牌為害怕發生)。<br/>⑩ ：結局 : 事情最終的結果。'
+        },
+        {
+            type: 'relationship_spread',
+            num: 10,
+            name: 'relationship'
+        },
+        {
+            type: 'zodiac_twelve_spread',
+            num: 12,
+            name: 'zodiac12'
+        },
+        {
+            type: 'zodiac_thirteen_spread',
+            num: 13,
+            name: 'zodiac13'
+        },
+        {
+            type: 'other_spread',
+            num: 0,
+            name: 'other'
+        }
+    ]
+    // global.language.Voyager.tw.card = majorArcana.concat(minorArcana)
+})(window)
