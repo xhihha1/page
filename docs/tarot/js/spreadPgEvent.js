5eb3c98573c018b935a621f7dde93f4aa2a6b9c9
yusen.yek <xhihha1@hotmail.com>
2022-05-05 23:01:27 +0800

    modify spread page

--- docs/tarot/js/spreadPgEvent.js
+++ docs/tarot/js/spreadPgEvent.js
@@ -26,18 +26,29 @@
     spreadPgEvent.prototype.setSpreadList = function (){
         const card = this.card
         let str = ''
+        let optionsStr = ''
         for (let i=0;i<card.spreadList.length;i++) {
             // const cardName = card.cardList[card.defaultOption.cardArray[i].index].name
             str += '<div class="spreadListCard">'
             // str += card.spreadList[i].type
-            str += '    <div>'+card.spreadList[i].type+'</div>'
+            str += '    <div>'+card.spreadList[i].name+'</div>'
             str += '    <img class="spreadImgCard" src="./tarot/img/spread/'+card.spreadList[i].name+'.png">'
             str += '</div>'
+
+            optionsStr += '<option value="'+i+'">'+card.spreadList[i].name+'</option>'
         }
         $('#product-slider').html(str)
+        $('#spreadSelect').html(optionsStr)
         this.resize()
         $('#product-slider').slick({
-            dots: true
+            dots: false
+        });
+        $('#product-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            $('#spreadSelect').val(nextSlide)
+        })
+        $('#spreadSelect').off("change")
+        $('#spreadSelect').on("change", function(){
+            $('#product-slider').slick('slickGoTo', parseInt($('#spreadSelect').val()))
         });
     }
     spreadPgEvent.prototype.resize = function () {
